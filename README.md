6af58e08d80a11548c6d9ec817c183efb118aa3e
thiagolimadyo <thiagolimadyo@outlook.com>
2025-11-19 19:29:29 -0300

    config. do projeto

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # arquitetura-limpa
+
 Projeto do curso Arquitetura Limpa do FormacaoDEV
+
+1. npm init -y
+2. npm i -D typescript
+3. npx tsc --init
+4. criar .gitignore
+5. criar pastas ./src e ./test
+6. npm i -D jest ts-jest @types/jest @types/node
+7. npm i -D ts-node-dev
+8. npx ts-jest config:init
---
+++ jest.config.js
@@ -0,0 +1,12 @@
+// const { createDefaultPreset } = require("ts-jest");
+import { createDefaultPreset } from "ts-jest";
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+export default {
+  testEnvironment: "node",
+  transform: {
+    ...tsJestTransformCfg,
+  },
+};
